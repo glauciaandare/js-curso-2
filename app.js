9c491e8577559deb607d190db35fc404b622b822
Gláucia Cecília Pivoto Andare <glaucia.andare@gmail.com>
2024-01-18 18:33:13 -0300

    finalizado projeto

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um numero de 1 a 10';
+
+function verificarChute(){
+    console.log('O botão foi clicado!')
+}
